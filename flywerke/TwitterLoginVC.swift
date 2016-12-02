9a79563e4ceffd83ba2a0b602cbdf824fd56c820
Paul Defilippi <BlindFaith@pauls-imac.home>
2016-12-02 01:03:15 -0500

    Can now login with Twitter and Auth with Firebase

--- flywerke/TwitterLoginVC.swift
+++ flywerke/TwitterLoginVC.swift
@@ -9,31 +9,52 @@
 import UIKit
 import Firebase
 import FirebaseAuth
+import TwitterKit
 
 class TwitterLoginVC: UIViewController {
     
+    var twitterLoginBtn: TWTRLogInButton?
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        addLoginBtn()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func addLoginBtn() {
+        
+        twitterLoginBtn = TWTRLogInButton(logInCompletion: { (session, error) in
+            
+            if (session != nil) {
+                print("session available")
+                self.connectWithTwitter(session: session!)
+            } else {
+                print(error?.localizedDescription as Any)
+            }
+        })
+        
+        twitterLoginBtn!.center = self.view.center
+        self.view.addSubview(twitterLoginBtn!)
     }
-    */
+    
+    func connectWithTwitter(session:TWTRSession) {
+        let credential = FIRTwitterAuthProvider.credential(withToken: session.authToken, secret: session.authTokenSecret)
+        
+        FIRAuth.auth()?.signIn(with: credential) { (user, error) in
+            
+            if error == nil {
+                print("\(user!.displayName!) successfully logged in")
+                self.performSegue(withIdentifier: "goToTweets", sender: self)
+            } else {
+                print(error?.localizedDescription as Any)
+            }
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+
+
 
 }
