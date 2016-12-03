51b1f2aad3156d9f81be63a021da8cf582408994
Paul Defilippi <BlindFaith@pauls-imac.home>
2016-12-02 21:52:27 -0500

    Added a toolbar above the keyboard

--- flywerke/TypeTweetVC.swift
+++ flywerke/TypeTweetVC.swift
@@ -12,18 +12,57 @@ class TypeTweetVC: UIViewController {
 
     @IBOutlet weak var tweetTextView: UITextView!
     
-    
-    
     @IBAction func closeAction(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        addToolBar(textView: tweetTextView)
 
-        // Do any additional setup after loading the view.
+    }
+    
+    func addToolBar(textView: UITextView) {
+        let toolbar = UIToolbar()
+        toolbar.barStyle = UIBarStyle.default
+        toolbar.isTranslucent = true
+        toolbar.tintColor = UIColor(red: 29 / 255.0, green: 160 / 255.0, blue: 242 / 255.0, alpha: 1.0)
+        
+        let doneBtn = UIBarButtonItem(title: "done", style: .plain, target: self, action: #selector(TypeTweetVC.doneAction))
+        
+        let cancelBtn = UIBarButtonItem(title: "cancel", style: .plain, target: self, action: #selector(TypeTweetVC.cancelAction))
+        
+        let spaceBtn = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
+        
+        // other control buttons
+        
+        toolbar.setItems([cancelBtn, spaceBtn, doneBtn], animated: false)
+        
+        toolbar.isUserInteractionEnabled = true
+        toolbar.sizeToFit()
+        
+        textView.inputAccessoryView = toolbar
+        
+        
+    }
+    
+    func doneAction() {
+        print("done")
+    }
+    
+    func cancelAction() {
+        print("cancel")
     }
 
+    func sendTweetAction() {
+        print("tweet sent!")
+    }
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
